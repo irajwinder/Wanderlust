082a3e074b52ef898fd45f6a8eabab3af2df578b
Rajwinder Singh <irajwinder@users.noreply.github.com>
2023-11-01 21:35:33 -0400

    First Commit

---
+++ Wanderlust/Registration/Register.swift
@@ -0,0 +1,18 @@
+//
+//  Register.swift
+//  Wanderlust
+//
+//  Created by Rajwinder Singh on 11/1/23.
+//
+
+import SwiftUI
+
+struct Register: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Register()
+}
--- Wanderlust/WanderlustApp.swift
+++ Wanderlust/WanderlustApp.swift
@@ -13,7 +13,7 @@ struct WanderlustApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Register()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
