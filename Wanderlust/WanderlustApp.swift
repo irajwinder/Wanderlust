99f2ff7422ab0e5bdf5720d0adfa041033e97e10
Rajwinder Singh <irajwinder@users.noreply.github.com>
2023-11-14 09:37:54 -0500

    Fixed code

--- Wanderlust/WanderlustApp.swift
+++ Wanderlust/WanderlustApp.swift
@@ -11,13 +11,12 @@ import SwiftUI
 struct WanderlustApp: App {
     //@StateObject private var dataController = DataController()
     @AppStorage("isLoggedIn") var isLoggedIn = true
-    let dataManagerInstance = DataManager.sharedInstance
+  
     
     var body: some Scene {
         WindowGroup {
             if isLoggedIn {
                 TabBarView()
-                        .environment(\.managedObjectContext, dataManagerInstance.persistentContainer.viewContext)
             } else {
                 Register()
             }
