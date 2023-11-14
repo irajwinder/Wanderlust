ef4a47fa426681938635866cbe7c7be0c84c0d6a
Rajwinder Singh <irajwinder@users.noreply.github.com>
2023-11-14 08:29:45 -0500

    Fixed error

--- Wanderlust/Photo/AddPhotoView.swift
+++ Wanderlust/Photo/AddPhotoView.swift
@@ -12,54 +12,89 @@ struct AddPhotoView: View {
     @Environment(\.dismiss) var dismiss
     @ObservedObject var viewModel: PhotoViewModel
     let selectedTrip: Trip?
-    @State private var galleryPhoto: String?
+   // @State private var galleryPhoto: String?
     @State private var photoTag: String = ""
     @State private var photoCaption: String = ""
     
     @State private var showAlert = false
     @State private var alert: Alert?
     
-    @State private var selectedPickerImage: PhotosPickerItem?
-    @State private var galleryPhotoImage: Image?
+//    @State private var selectedPickerImage: PhotosPickerItem?
+//    @State private var galleryPhotoImage: Image?
+    
+    @State private var selectedPickerImages: [PhotosPickerItem] = []
+    @State private var galleryPhotoImages: [Image] = []
+    @State private var galleryPhotos: [String] = []
     
     var body: some View {
         VStack {
             Form {
                 Section(header: Text("Trip Photo(s)")) {
                     HStack {
-                        CustomText(text: "Journal Picture", textSize: 20, textColor: .black)
+                        CustomText(text: "Trip Pictures", textSize: 20, textColor: .black)
                         Spacer()
                         
                         PhotosPicker(
                             "Select photo",
-                            selection: $selectedPickerImage,
+                            selection: $selectedPickerImages,
+                            maxSelectionCount: 5,
                             matching: .images
                         )
                         
-                    }.onChange(of: selectedPickerImage) {
+                    }.onChange(of: selectedPickerImages) { selectedItems in
                         Task {
-                            if let data = try? await selectedPickerImage?.loadTransferable(type: Data.self) {
-                                if let uiImage = UIImage(data: data) {
-                                    galleryPhotoImage = Image(uiImage: uiImage)
-                                    // Save image to file manager and get the URL
-                                    if let imageURL = saveImageToFileManager(uiImage) {
-                                        galleryPhoto = imageURL
+                            for selectedItem in selectedItems {
+                                if let data = try? await selectedItem.loadTransferable(type: Data.self) {
+                                    if let uiImage = UIImage(data: data) {
+                                        let galleryImage = Image(uiImage: uiImage)
+                                        galleryPhotoImages.append(galleryImage)
+
+                                        // Save image to file manager and get the URL
+                                        if let imageURL = saveImageToFileManager(uiImage) {
+                                            galleryPhotos.append(imageURL)
+                                        }
                                     }
-                                    return
                                 }
                             }
-                            print("Failed")
+                        }
+                    }
+
+                    LazyVGrid(columns: [
+                        GridItem(.adaptive(minimum: 100)),
+                        GridItem(.adaptive(minimum: 100))
+                    ]) {
+                        ForEach(galleryPhotoImages.indices, id: \.self) { index in
+                            galleryPhotoImages[index]
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
                         }
                     }
                     
-                    VStack {
-                        if let galleryPhotoImage {
-                            galleryPhotoImage
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50, height: 50)                            
-                        }
-                    }
+//                    .onChange(of: selectedPickerImage) {
+//                        Task {
+//                            if let data = try? await selectedPickerImage?.loadTransferable(type: Data.self) {
+//                                if let uiImage = UIImage(data: data) {
+//                                    galleryPhotoImages = Image(uiImage: uiImage)
+//                                    // Save image to file manager and get the URL
+//                                    if let imageURL = saveImageToFileManager(uiImage) {
+//                                        galleryPhoto = imageURL
+//                                    }
+//                                    return
+//                                }
+//                            }
+//                            print("Failed")
+//                        }
+//                    }
+                    
+//                    VStack {
+//                        if let galleryPhotoImage {
+//                            galleryPhotoImage
+//                                .resizable()
+//                                .scaledToFit()
+//                                .frame(width: 50, height: 50)                            
+//                        }
+//                    }
                     
                     HStack {
                         CustomText(text: "Photo Caption", textSize: 20, textColor: .black)
@@ -76,7 +111,7 @@ struct AddPhotoView: View {
             }
         }.navigationBarTitle("Add Photo")
             .toolbar {
-                ToolbarItem {
+                ToolbarItem(placement: .navigationBarTrailing) {
                     Button("Save", action: {
                         SaveAndValidatePhoto()
                     })
@@ -101,7 +136,7 @@ struct AddPhotoView: View {
             return
         }
         
-        guard let galleryPhoto = galleryPhoto else {
+        guard !galleryPhotos.isEmpty else {
             showAlert = true
             alert = Validation.showAlert(title: "Error", message: "Please select trip photos")
             return
@@ -113,12 +148,14 @@ struct AddPhotoView: View {
         }
         
         // Save Photo Details
-        dataManagerInstance.savePhoto(
-            tripName: selectedTrip,
-            imageData: galleryPhoto,
-            photoCaption: photoCaption,
-            photoTag: photoTag
-        )
+        for photoURL in galleryPhotos {
+            dataManagerInstance.savePhoto(
+                tripName: selectedTrip,
+                imageData: photoURL,
+                photoCaption: photoCaption,
+                photoTag: photoTag
+            )
+        }
         
         // Show a success alert
         showAlert = true
@@ -138,7 +175,7 @@ struct AddPhotoView: View {
         }
 
         let folderName = "GalleryPictures"
-        let fileName = "\(UUID.self).jpg"
+        let fileName = "\(UUID().uuidString).jpg"
         let relativeURL  = "\(folderName)/\(fileName)"
 
         do {
