b6ff502aa5086331ad082398f9763ee3c42c019d
Rajwinder Singh <irajwinder@users.noreply.github.com>
2023-11-14 23:25:54 -0500

    Bug fixed

--- Wanderlust/WanderlustApp.swift
+++ Wanderlust/WanderlustApp.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 @main
 struct WanderlustApp: App {
-    @AppStorage("isLoggedIn") var isLoggedIn = true
+    @AppStorage("isLoggedIn") var isLoggedIn = false
     
     var body: some Scene {
         WindowGroup {
-            if isLoggedIn {
+            if isLoggedIn == true {
                 TabBarView()
             } else {
                 Register()
